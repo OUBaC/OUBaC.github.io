26bd2cbbbbb64a565ae78d68f35ded931aada7c1
Tom-Johnston <tom.johnston5861@gmail.com>
2015-05-16 14:44:38 +0100

    The body is now fixed when the side nav is out.

--- javascript.js
+++ javascript.js
@@ -18,8 +18,10 @@ $(function() {
 function expandSideNav(){
 		document.getElementById('navigation-drawer').style.left="0px";
 		document.getElementById('small-grey-out').style.width="100%";
+		document.getElementsByTagName('body')[0].style.position="fixed";
 }
 function hideSideNav(){
 		document.getElementById('navigation-drawer').style.left="-300px";
-		document.getElementById('small-grey-out').style.width="0%";	
+		document.getElementById('small-grey-out').style.width="0%";
+		document.getElementsByTagName('body')[0].style.position="static";
 }
