298068998377d3160e7395e34ff3fc9ddb3aba8f
Tom-Johnston <tom.johnston5861@gmail.com>
2015-09-26 17:30:58 +0100

    Neatened code.

--- javascript.js
+++ javascript.js
@@ -10,10 +10,8 @@ function scrollAdjuster(){
   var scrollDistance = document.body.scrollTop;
   if(scrollDistance>=startHeight-changeHeight){
     document.getElementById('header').classList.add('smaller');
-    document.getElementById('top-nav-container').classList.add('smaller-top-nav-container');
   }else{
     document.getElementById('header').classList.remove('smaller');
-    document.getElementById('top-nav-container').classList.remove('smaller-top-nav-container');
   }
   if(scrollDistance>=startHeight-stopHeight){
     document.getElementById('header').classList.add('box-shadow');
